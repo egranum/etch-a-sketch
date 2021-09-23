4457104e46b575f909fe161d5a5181d6bd0f1dd1
egranum <elliot.granum@ways.no>
2021-09-23 09:37:25 +0200

    Make grid adjustable, add reset, add user input

--- script.js
+++ script.js
@@ -2,7 +2,21 @@ window.onload = () => {
     const containerDiv = document.querySelector('.container');
     let newDiv;
 
-    for(let i = 0; i < 16; i++) {
+    const inputNumber = Number(prompt('insert squares per side', ''));
+
+    if (inputNumber > 100) {
+        alert('not that many!');
+        location.reload();
+    }
+
+    const style = document.documentElement.style;
+
+    style.setProperty('--colNum', inputNumber);
+    style.setProperty('--rowNum', inputNumber);
+
+    const squares = inputNumber * inputNumber;
+
+    for(let i = 0; i < squares; i++) {
         newDiv = document.createElement('div');
         newDiv.classList.add('grid-item');
         newDiv.id = `grid-item-${i + 1}`;
@@ -14,7 +28,13 @@ const containerDiv = document.querySelector('.container');
 
 containerDiv.addEventListener('mouseover', function(event) {
     if(event.target.className.toLowerCase() === 'grid-item') {
-        console.log(event.target);
         event.target.classList.add('grid-item-active');
     }
 })
+
+const resetButton = document.querySelector('.reset-button');
+const refreshPage = () => {
+    location.reload();
+}
+
+resetButton.addEventListener('click', refreshPage)
